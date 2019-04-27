f3022176f56023cd95d9fe7dec7802b226a73794
Billy Bunn <williammbunn@gmail.com>
2019-04-27 16:56:51 -0700

    adds placeholder for map img

--- src/components/map.js
+++ src/components/map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Map() {
-  return <img id="map" className="hide" src="" alt="Map of search query" />
+  return <img id="map" className="hide" src="https://via.placeholder.com/400" alt="Map of search query" />
 };
 
 export default Map;
